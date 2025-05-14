60ea85b68d7d1ca7ebe36d3b85d3b621e5a5c934
Kevin Assassin <kassassi@f4r13s16.clusters.42paris.fr>
2025-05-14 16:40:01 +0200

    Save sur le repo perso

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kassassi <kassassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 11:14:40 by kassassi          #+#    #+#             */
-/*   Updated: 2025/05/13 16:58:39 by kassassi         ###   ########.fr       */
+/*   Updated: 2025/05/14 15:53:27 by kassassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,10 @@ typedef struct s_list
 t_list	*ft_lstnew(void	*content);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 int		ft_lstsize(t_list *lst);
 
 #endif
